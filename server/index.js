73fa772f3f152933812bd6a2d43df039956b447b
ilan <ilan20008@gmail.com>
2020-12-20 19:05:01 +0200

    deploy fix

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const path = require('path');
 
 const { connect } = require('./db');
 connect();
@@ -13,4 +14,9 @@ app.use(express.json());
 app.use("/api/todos", todosRoute);
 app.use("/", express.static(path.join(__dirname, '../client/build')));
 
-app.listen(process.env.PORT || 3001);
+let port = 3001;
+if(process.env.PORT) {
+    port = process.env.PORT;
+}
+
+app.listen(port);
